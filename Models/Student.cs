c5430043ac00e61beb6deb75e6381c4b678ca3b7
dathoccode <datphungkl@gmail.com>
2025-09-21 11:36:25 +0700

    annotation

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,15 +1,29 @@
-﻿namespace QuanLySinhVien.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuanLySinhVien.Models
 {
     public class Student
     {
         public int Id { get; set; }
+        [Required]
         public string? Name { get; set; }
+        [Required(ErrorMessage = "Bắt buộc phải nhập email")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Email không hợp lệ")]
         public string? Email { get; set; }
+        [StringLength(100, MinimumLength = 8)]
+        [Required]
         public string? Password { get; set; }
+        [Required]
         public Branch? Branch { get; set; }
+        [Required]
         public Gender? Gender { get; set; }
         public bool IsRegular { get; set; }
+        [DataType(DataType.MultilineText)]
+        [Required]
         public string? Address { get; set; }
+        [DataType(DataType.Date)]
+        [Required]
+        [Range(typeof(DateTime), "1/1/1963", "12/31/2025")]
         public DateTime DateOfBirth { get; set; }
         public string? ImagePath { get; set; }
     }
